2a88717745dc19e16a41ad53417cf3695059a9dc
Mathieu <mathieurousseau123@gmail.com>
2021-04-26 16:07:52 -0700

    completed question 4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,7 +6,7 @@ puts "----------"
 # Your code goes below here ...
 
 store_burnaby = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-store_richmond = Store.create(name: 'Richmond', annual_revenue: 126000, mens_apparel: false, womens_apparel: true)
+store_richmond = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 store_gastown = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts "The new store count is: #{Store.count}"
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store_Surrey = Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store_Whistler = Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store_Yaletown = Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts "--- Stores that carry men's apparel ---"
+@mens_stores.each do |store|
+  puts "The store in #{store.name} has an annual revenue of #{store.annual_revenue}."
+end
+
+@womens_stores = Store.where(womens_apparel: true)
+puts "--- Stores that carry women's apparel, with less than a million dollars in revenue ---"
+@womens_stores.each do |store|
+  if store.annual_revenue < 1000000
+    puts "The store in #{store.name} has an annual revenue of #{store.annual_revenue}."
+  end
+end
